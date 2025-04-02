f38bc2a9f67e8da12ab447e2f58575242b595530
NastyaTsimanovich97 <nastya.cimanovich.97@mail.ru>
2025-04-02 17:26:07 +0300

    feat: implement url input

--- src/app/[locale]/client/page.tsx
+++ src/app/[locale]/client/page.tsx
@@ -1,6 +1,7 @@
+import { getTranslations } from "next-intl/server";
+import Input from "@/app/_components/Input";
 import Select from "@/app/_components/Select";
 import { Methods } from "@/app/_constants/methods";
-import { getTranslations } from "next-intl/server";
 
 const SelectOptions = Methods.map((item) => ({ label: item, value: item }));
 
@@ -8,9 +9,12 @@ export default async function ClientPage() {
   const t = await getTranslations("Client_Page");
 
   return (
-    <div>
+    <section className="flex flex-col items-center py-6">
       <h3>{t("REST_Client")}</h3>
-      <Select label={t("Method")} options={SelectOptions} />
-    </div>
+      <div className="flex items-end gap-4">
+        <Select label={t("Method")} options={SelectOptions} />
+        <Input label={t("Endpoint_URL")} id="url" />
+      </div>
+    </section>
   );
 }
