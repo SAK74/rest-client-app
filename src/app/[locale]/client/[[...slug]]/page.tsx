b64d45ff819fc57993a3c71fb1882d8c9738eefb
NastyaTsimanovich97 <nastya.cimanovich.97@mail.ru>
2025-04-05 17:56:39 +0300

    feat: add default code and body

--- src/app/[locale]/client/[[...slug]]/page.tsx
+++ src/app/[locale]/client/[[...slug]]/page.tsx
@@ -2,6 +2,8 @@ import { getTranslations } from "next-intl/server";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Headers from "../_components/Headers";
 import InputUrl from "../_components/InputUrl";
+import Code from "../_components/Code";
+import Body from "../_components/Body";
 
 export default async function ClientPage() {
   const t = await getTranslations("Client_Page");
@@ -17,6 +19,8 @@ export default async function ClientPage() {
         <CardContent className="flex flex-col gap-8">
           <InputUrl />
           <Headers />
+          <Code />
+          <Body />
         </CardContent>
       </Card>
       <Card className="mx-6 w-4xl">
---
+++ src/app/[locale]/client/_components/Body.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Body() {
+  return (
+    <div>
+      <CardHeader className="px-0">
+        <CardTitle>Body:</CardTitle>
+      </CardHeader>
+      <CardContent className="px-0 py-6"></CardContent>
+    </div>
+  );
+}
