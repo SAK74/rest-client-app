3c78155d091e792b819dd3ecf9a67b875e634c89
SAK74 <sergijks@gmail.com>
2025-04-09 12:55:00 +0200

    feat: initial add tabs for client page

--- src/app/[locale]/client/[...request]/page.tsx
+++ src/app/[locale]/client/[...request]/page.tsx
@@ -9,6 +9,7 @@ import BodyEditor from "../_components/BodyEditor";
 import ResponseView from "../_components/ResponseView";
 import CodeViewer from "../_components/CodeView";
 import { usePathname, useRouter } from "@/i18n/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 // try to type http://localhost:3000/client/POST/aHR0cHM6Ly9qc29ucGxhY2Vob2xkZXIudHlwaWNvZGUuY29tL3Bvc3Rz/eyJ0aXRsZSI6ImZha2VUaXRsZSIsInVzZXJJZCI6MSwiYm9keSI6ImZha2VNZXNzYWdlIn0=?Content-Type=application%2Fjson
 
@@ -65,14 +66,31 @@ export default function Page({
 
   return (
     <main className="flex flex-col gap-6">
-      <section className="flex gap-6">
+      <section className="flex flex-col gap-6">
         {/* Request section */}
-        <Method method={method} onMethodChange={onMethodChange} />
-        <UrlInput decodedUrl={urlDecoded} onUrlChange={onUrlChange} />
-        <HeadersHandler _headers={_headers} />
-        <BodyEditor decodedBody={bodyDecoded} />
-        <CodeViewer />
-        <Button onClick={onGo}>Go!</Button>
+        <div className="flex gap-6">
+          <Method method={method} onMethodChange={onMethodChange} />
+          <UrlInput decodedUrl={urlDecoded} onUrlChange={onUrlChange} />
+          {/* <CodeViewer /> */}
+          <Button onClick={onGo}>Go!</Button>
+        </div>
+
+        <Tabs defaultValue={"headers"}>
+          <TabsList>
+            <TabsTrigger value="headers">Headers</TabsTrigger>
+            <TabsTrigger value="body">Body</TabsTrigger>
+            <TabsTrigger value="code">Code</TabsTrigger>
+          </TabsList>
+          <TabsContent value="headers">
+            <HeadersHandler _headers={_headers} />
+          </TabsContent>
+          <TabsContent value="body">
+            <BodyEditor decodedBody={bodyDecoded} />
+          </TabsContent>
+          <TabsContent value="code">
+            <CodeViewer />
+          </TabsContent>
+        </Tabs>
       </section>
       <section>
         <ResponseView response={response} />
