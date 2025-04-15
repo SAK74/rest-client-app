6471e14cfccb49e086cb8d45813c6d2e2403eed4
NastyaTsimanovich97 <nastya.cimanovich.97@mail.ru>
2025-04-15 12:37:53 +0300

    feat: add history list with links to page

--- src/app/[locale]/history/_components/Client.tsx
+++ src/app/[locale]/history/_components/Client.tsx
@@ -1,14 +1,42 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 import { useLocalStorage } from "@/lib/hooks/useLocalStorage";
-import NoRequests from "./NoRequests";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import NoRequests from "./NoRequests";
+import { Button } from "@/components/ui/button";
+import { getFullClientLink } from "@/lib/getFullClientLink";
+
+export interface HistoryItem {
+  method: string;
+  url: string;
+  body: string;
+  headers: { [k: string]: string };
+  executionTime: number;
+}
 
 export default function ClientPage() {
   const t = useTranslations("History_Page");
 
   const [history] = useLocalStorage("history");
+
+  const [sortedHistory, setSortedHistory] = useState<HistoryItem[]>();
+
+  useEffect(() => {
+    if (!!history.length) {
+      try {
+        const value: [] = JSON.parse(history);
+        const sortedValue = value.sort(
+          (a: HistoryItem, b: HistoryItem) => b.executionTime - a.executionTime,
+        );
+        setSortedHistory(sortedValue);
+      } catch (error) {
+        console.error("Not able to parse history JSON", error);
+      }
+    }
+  }, [history]);
 
   return (
     <main className="flex flex-col gap-8 py-4 items-center">
@@ -19,7 +47,19 @@ export default function ClientPage() {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          {!history?.length ? <NoRequests t={t} /> : <></>}
+          {!sortedHistory?.length ? (
+            <NoRequests t={t} />
+          ) : (
+            <div className="flex flex-col">
+              {sortedHistory.map((item) => (
+                <Link key={item.executionTime} href={getFullClientLink(item)}>
+                  <Button variant="link">
+                    {item.method} {item.url}
+                  </Button>
+                </Link>
+              ))}
+            </div>
+          )}
         </CardContent>
       </Card>
     </main>
---
+++ src/lib/getFullClientLink.ts
@@ -0,0 +1,25 @@
+import { HistoryItem } from "@/app/[locale]/history/_components/Client";
+
+export function getFullClientLink(item: HistoryItem): string {
+  let link = `client/${item.method}/${btoa(item.url)}`;
+
+  if (item.body) {
+    link += `/${btoa(item.body)}`;
+  }
+
+  if (item.headers) {
+    let searchParams = "?";
+    const headers = Object.entries(item.headers).map(([key, value]) => ({
+      key,
+      value,
+    }));
+
+    headers.forEach(({ key, value }) => {
+      searchParams += `${key}=${value}`;
+    });
+
+    link += searchParams;
+  }
+
+  return link;
+}
