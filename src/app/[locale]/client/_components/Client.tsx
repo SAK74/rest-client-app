d40654ac214bc91dca14b9aa1accec8ad53f4cc8
NastyaTsimanovich97 <nastya.cimanovich.97@mail.ru>
2025-04-16 15:42:16 +0300

    fix: remove console.warn on body parser
    fix: minor fixes

--- src/app/[locale]/client/_components/Client.tsx
+++ src/app/[locale]/client/_components/Client.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useParams, useSearchParams } from "next/navigation";
 import { useTranslations } from "next-intl";
-import { Link, usePathname, useRouter } from "@/i18n/navigation";
+import { usePathname, useRouter } from "@/i18n/navigation";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
@@ -147,10 +147,6 @@ export default function ClientPage() {
           <CardContent></CardContent>
         </Card>
       </section>
-
-      <Link href="/variables" className="mt-8 p-4">
-        <button>Go to Variables!!!</button>
-      </Link>
     </main>
   );
 }
